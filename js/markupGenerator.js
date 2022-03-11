d82e3b57350656f40e2f0b58081ac2fcc315957e
Artur Malyshev <42480216+ArturMalyshev@users.noreply.github.com>
2022-03-11 09:43:10 +0300

    все сделал, только не понял какую я проверку делал на фотографии...(На фотографии ты сделал проверку молодец, а на остальные поля тоже надо сделать!)

--- js/markupGenerator.js
+++ js/markupGenerator.js
@@ -1,40 +1,30 @@
 import {getLittleOffer} from './offer-util.js';
+import {findEmptyField, getOfferTypeTranslate} from './markupGeneratorFunctions.js';
 const data = getLittleOffer(1);
 
 const cardBlock = document.querySelector('#map-canvas');
 const cardTemplate = document.querySelector('#card').content;
-data.forEach((offer) => {
+data.forEach((offerElement) => {
   const card = cardTemplate.cloneNode(true);
-  card.querySelector('.popup__title').textContent = offer.offer.title;
-  card.querySelector('.popup__text--address').textContent = offer.offer.address;
-  card.querySelector('.popup__text--price').textContent = `${offer.offer.price  } ₽/ночь`;
-  if (offer.offer.type === 'palace') {
-    card.querySelector('.popup__type').textContent = 'Дворец';
-  } else if (offer.offer.type === 'flat') {
-    card.querySelector('.popup__type').textContent = 'Квартира';
-  } else if (offer.offer.type === 'house') {
-    card.querySelector('.popup__type').textContent = 'Дом';
-  } else if (offer.offer.type === 'bungalow') {
-    card.querySelector('.popup__type').textContent = 'Бунгало';
-  } else if (offer.offer.type === 'hotel') {
-    card.querySelector('.popup__type').textContent = 'Отель';
-  }
-  card.querySelector('.popup__text--capacity').textContent = `${offer.offer.rooms  } комнаты для ${offer.offer.guests} гостей`;
-  card.querySelector('.popup__text--time').textContent = `${offer.offer.checkin}, выезд до ${offer.offer.checkout}`;
-  card.querySelector('.popup__features').textContent = offer.offer.features.join(', ');
-  card.querySelector('.popup__description').textContent = offer.offer.description;
+  card.querySelector('.popup__title').textContent = offerElement.offer.title;
+  card.querySelector('.popup__text--address').textContent = offerElement.offer.address;
+  card.querySelector('.popup__text--price').textContent = `${offerElement.offer.price  } ₽/ночь`;
+  card.querySelector('.popup__type').textContent = getOfferTypeTranslate(offerElement.offer.type);
+  card.querySelector('.popup__text--capacity').textContent = `${offerElement.offer.rooms  } комнаты для ${offerElement.offer.guests} гостей`;
+  card.querySelector('.popup__text--time').textContent = `${offerElement.offer.checkin}, выезд до ${offerElement.offer.checkout}`;
+  card.querySelector('.popup__features').textContent = offerElement.offer.features.join(', ');
+  card.querySelector('.popup__description').textContent = offerElement.offer.description;
   {
     const photoGallery = card.querySelector('.popup__photos');
     let element;
-    for (element in offer.offer.photos) {
+    for (element in offerElement.offer.photos) {
       const photo = photoGallery.children[0].cloneNode(true);
-      photo.src = offer.offer.photos[element];
+      photo.src = offerElement.offer.photos[element];
       photoGallery.appendChild(photo);
     }
-    photoGallery.children[0].remove();
   }
-  card.querySelector('.popup__avatar').src = offer.author.avatar;
+  card.querySelector('.popup__avatar').src = offerElement.author.avatar;
+  findEmptyField(card.querySelector('.popup'));
   cardBlock.appendChild(card);
 });
-
 
